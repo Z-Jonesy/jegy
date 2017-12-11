e2b4a0a1762345f70ebdc5cbfcdfe1d05e094f9e
zozo <zoltan_bogdan@live.com>
2017-12-11 20:02:09 +0100

    (modify): firebase deploy -> jegybazar-203f0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,8 @@ import 'moment/locale/hu';
 import {TicketBidFormComponent} from './ticket/ticket-bid-form/ticket-bid-form.component';
 import {LoadingSpinnerComponent} from './core/loading-spinner/loading-spinner.component';
 import {BidService} from './shared/bid.service';
+import * as firebase from 'firebase';
+import {environment} from "../environments/environment";
 
 @NgModule({
   declarations: [
@@ -62,4 +64,7 @@ import {BidService} from './shared/bid.service';
   bootstrap: [AppComponent]
 })
 export class AppModule {
+  constructor() {
+    firebase.initializeApp(environment.firebase);
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,14 @@
 export const environment = {
   production: true,
   firebase: {
-    baseUrl: 'https://jegybazar-27302.firebaseio.com',
+    baseUrl: 'https://jegybazar-203f0.firebaseio.com',
     registrationUrl: ' https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser',
     loginUrl: ' https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword',
-    apikey: 'AIzaSyALPBS-gllA6F-vRya-6pYZXABheOWFYlw'
+    apiKey: 'AIzaSyD7sKNXe1DkvVYxXsXOxUw8Qq450b4jR5o',
+    authDomain: 'jegybazar-203f0.firebaseapp.com',
+    databaseURL: 'https://jegybazar-203f0.firebaseio.com',
+    projectId: 'jegybazar-203f0',
+    storageBucket: 'jegybazar-203f0.appspot.com',
+    messagingSenderId: '280419090188'
   }
 };
