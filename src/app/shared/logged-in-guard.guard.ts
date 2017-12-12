3f4c0acb3236e00e13f3b2b0fd82e0f3fdd1ba76
zozo <zoltan_bogdan@live.com>
2017-12-12 12:22:14 +0100

    (modify):  rename isLoggedIn and modify loggedinGuard

--- src/app/shared/logged-in-guard.guard.ts
+++ src/app/shared/logged-in-guard.guard.ts
@@ -1,32 +1,25 @@
-import { Location } from '@angular/common';
-import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot
-} from '@angular/router';
-import { Observable } from 'rxjs/Observable';
-import { UserService } from './user.service';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {Observable} from 'rxjs/Observable';
+import {UserService} from './user.service';
 
 @Injectable()
 export class LoggedInGuardGuard implements CanActivate {
   constructor(private _userService: UserService,
-              private _router: Router,
-              private _location: Location) {
+              private _router: Router) {
   }
 
   canActivate(next: ActivatedRouteSnapshot,
               state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    if (this._userService.isLoggedin) {
-      return true;
-    } else {
-      // itt annyit csinaltunk, hogy a Location segitsegevel
-      // nem elnavigalunk valami fix helyre ha nincs jogunk
-      // hanem pusztan helyben maradunk
-      // illetve indulasnal alap routeba esunk bele
-      this._router.navigate([this._location.path()]);
-      return false;
-    }
+    return this._userService.isLoggedIn$.map(
+      isLoggedIN => {
+        if (isLoggedIN === false) {
+          this._router.navigate(['/home']);
+          return false;
+        }
+        return true;
+      }
+    );
   }
 }
+
